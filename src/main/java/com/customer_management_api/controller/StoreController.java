8fe8379e34b45e9195987cff13c607884d22acde
星野龍 <ryu0114h@gmail.com>
2022-05-24 09:10:25 +0900

    create store controller

---
+++ src/main/java/com/customer_management_api/controller/StoreController.java
@@ -0,0 +1,14 @@
+package com.customer_management_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stores")
+public class StoreController {
+    @GetMapping
+    public String getStores() {
+        return "get stores";
+    }
+}
