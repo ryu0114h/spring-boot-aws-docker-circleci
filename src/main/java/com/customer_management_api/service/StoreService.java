4c5c55cc4e6e7e25d4c1cab9fcb120651e00510f
星野龍 <ryu0114h@gmail.com>
2022-05-24 09:13:49 +0900

    create store service

---
+++ src/main/java/com/customer_management_api/service/StoreService.java
@@ -0,0 +1,8 @@
+package com.customer_management_api.service;
+
+import com.customer_management_api.entity.Store;
+import java.util.List;
+
+public interface StoreService {
+    List<Store> getStores();
+}
---
+++ src/main/java/com/customer_management_api/service/StoreServiceImpl.java
@@ -0,0 +1,13 @@
+package com.customer_management_api.service;
+
+import com.customer_management_api.entity.Store;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StoreServiceImpl implements StoreService {
+    @Override
+    public List<Store> getStores() {
+        return null;
+    }
+}
