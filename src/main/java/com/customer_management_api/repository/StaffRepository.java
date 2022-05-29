88a388206223d086253c52e86e4623c1be370983
星野龍 <ryu0114h@gmail.com>
2022-05-29 09:56:35 +0900

    create staff repo

---
+++ src/main/java/com/customer_management_api/repository/StaffRepository.java
@@ -0,0 +1,18 @@
+package com.customer_management_api.repository;
+
+import com.customer_management_api.entity.Staff;
+import java.util.List;
+import java.util.Optional;
+
+public interface StaffRepository {
+
+    List<Staff> getStaffs();
+
+    Optional<Staff> getStaff(Long id);
+
+    Staff createStaff(Staff staff);
+
+    Staff updateStaff(Staff staff);
+
+    void deleteStaff(Long id);
+}
---
+++ src/main/java/com/customer_management_api/repository/StaffRepositoryImpl.java
@@ -0,0 +1,43 @@
+package com.customer_management_api.repository;
+
+import com.customer_management_api.entity.Staff;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StaffRepositoryImpl implements StaffRepository {
+
+    private final StaffMapper staffMapper;
+
+    @Autowired
+    public StaffRepositoryImpl(StaffMapper staffMapper) {
+        this.staffMapper = staffMapper;
+    }
+
+    @Override
+    public List<Staff> getStaffs() {
+        return staffMapper.getStaffs();
+    }
+
+    @Override
+    public Optional<Staff> getStaff(Long id) {
+        return staffMapper.getStaff(id);
+    }
+
+    @Override
+    public Staff createStaff(Staff staff) {
+        return staffMapper.createStaff(staff);
+    }
+
+    @Override
+    public Staff updateStaff(Staff staff) {
+        return staffMapper.updateStaff(staff);
+    }
+
+    @Override
+    public void deleteStaff(Long id) {
+        staffMapper.deleteStaff(id);
+    }
+}
