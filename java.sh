8e433f75cab64774ab6595a4fcf2080524ede282
星野龍 <ryu0114h@gmail.com>
2022-06-28 08:26:28 +0900

    fix java.sh export

--- java.sh
+++ java.sh
@@ -2,6 +2,9 @@
 
 JAR_FILE_NAME=$1
 LOG_FILE_NAME=java.log
-#sudo nohup java -jar $JAR_FILE_NAME --spring.profiles.active=production > $LOG_FILE_NAME &
-sudo nohup java -jar $JAR_FILE_NAME --spring.profiles.active=production > -c $LOG_FILE_NAME &
 
+export DB_ENDPOINT=customer-management-database-1.cluster-c37ecyruueoq.ap-northeast-1.rds.amazonaws.com
+export DB_USERNAME=admin
+export DB_PASSWORD=password
+
+sudo nohup java -jar $JAR_FILE_NAME --spring.profiles.active=production > -c $LOG_FILE_NAME &
