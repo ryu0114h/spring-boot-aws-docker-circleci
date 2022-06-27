c8e840618f2a18bcefa9d0a1e9d6a87a09c3f0a1
星野龍 <ryu0114h@gmail.com>
2022-06-28 08:20:30 +0900

    fix java.sh

--- java.sh
+++ java.sh
@@ -2,5 +2,6 @@
 
 JAR_FILE_NAME=$1
 LOG_FILE_NAME=java.log
-sudo nohup java -jar $JAR_FILE_NAME --spring.profiles.active=production > $LOG_FILE_NAME &
+#sudo nohup java -jar $JAR_FILE_NAME --spring.profiles.active=production > $LOG_FILE_NAME &
+sudo nohup java -jar $JAR_FILE_NAME --spring.profiles.active=production > -c $LOG_FILE_NAME &
 
