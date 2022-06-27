6d2f96e7da567229e6d32b2d46702cf6109c0501
星野龍 <ryu0114h@gmail.com>
2022-06-28 08:38:40 +0900

    fix java.sh

--- java.sh
+++ java.sh
@@ -7,4 +7,11 @@ export DB_ENDPOINT=customer-management-database-1.cluster-c37ecyruueoq.ap-northe
 export DB_USERNAME=admin
 export DB_PASSWORD=password
 
+if [ -e $LOG_FILE_NAME ]; then
+  echo "File exists."
+else
+  touch $LOG_FILE_NAME
+  sudo chmod 777 $LOG_FILE_NAME
+fi
+
 sudo nohup java -jar $JAR_FILE_NAME --spring.profiles.active=production > $LOG_FILE_NAME &
