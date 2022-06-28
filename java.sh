7fa64e4395473f266fe2901e2861f7d9dfa8b78a
星野龍 <ryu0114h@gmail.com>
2022-06-29 07:45:58 +0900

    fix コメントアウト

--- java.sh
+++ java.sh
@@ -3,11 +3,13 @@
 JAR_FILE_NAME=$1
 LOG_FILE_NAME=java.log
 
+#　環境変数の設定
 export DB_ENDPOINT=customer-management-database-1.cluster-c37ecyruueoq.ap-northeast-1.rds.amazonaws.com
 export DB_USERNAME=admin
 export DB_PASSWORD=password
 echo "set env"
 
+# ログファイルがなければ作成
 if [ -e $LOG_FILE_NAME ]; then
   echo "file exists"
 else
@@ -16,7 +18,6 @@ else
   echo "create a log file"
 fi
 
-ps aux | grep customer-management-api-0.0.1-SNAPSHOT.jar | grep -v grep
 # 既に動いていたら切断
 java_pid=`ps aux | grep 'java -jar customer-management-api-0.0.1-SNAPSHOT.jar --spring.profiles.active=production' | grep -v grep | awk '{ print $2 }'`
 echo $java_pid
@@ -25,5 +26,6 @@ if [ $java_pid ]; then
   echo "kill java process"
 fi
 
+# Spring Boot を実行
 nohup java -jar $JAR_FILE_NAME --spring.profiles.active=production > $LOG_FILE_NAME &
-echo "java -jar"
+echo "run spring boot"
